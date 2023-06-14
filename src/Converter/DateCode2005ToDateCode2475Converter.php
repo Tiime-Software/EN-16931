145ef4e569f370803a9c0feb06275f43351f647f
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-06-14 16:48:06 +0200

    fix cs

--- src/Converter/DateCode2005ToDateCode2475Converter.php
+++ src/Converter/DateCode2005ToDateCode2475Converter.php
@@ -11,7 +11,7 @@ class DateCode2005ToDateCode2475Converter
 {
     public static function convert(DateCode2005 $dateCode2005): DateCode2475
     {
-        return match($dateCode2005) {
+        return match ($dateCode2005) {
             DateCode2005::INVOICE_DATE => DateCode2475::INVOICE_DATE,
             DateCode2005::DELIVERY_DATE => DateCode2475::DELIVERY_DATE,
             DateCode2005::PAYMENT_DATE => DateCode2475::PAYMENT_DATE
