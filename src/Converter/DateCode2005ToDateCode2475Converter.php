4eae62588f31bb759165fbe6646e0ba3bc2d1f4b
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-06-14 16:46:13 +0200

    add converter from DateCode2005 to DateCode2475

---
+++ src/Converter/DateCode2005ToDateCode2475Converter.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tiime\EN16931\Converter;
+
+use Tiime\EN16931\DataType\DateCode2005;
+use Tiime\EN16931\DataType\DateCode2475;
+
+class DateCode2005ToDateCode2475Converter
+{
+    public static function convert(DateCode2005 $dateCode2005): DateCode2475
+    {
+        return match($dateCode2005) {
+            DateCode2005::INVOICE_DATE => DateCode2475::INVOICE_DATE,
+            DateCode2005::DELIVERY_DATE => DateCode2475::DELIVERY_DATE,
+            DateCode2005::PAYMENT_DATE => DateCode2475::PAYMENT_DATE
+        };
+    }
+}
