26dcd5a20ddafb1c91632dc77e1f076c88f74aa4
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2024-10-15 16:00:42 +0200

    remove deprecated code

--- src/DataType/Identifier/ItemClassificationIdentifier.php
+++ src/DataType/Identifier/ItemClassificationIdentifier.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tiime\EN16931\DataType\Identifier;
 
-use Tiime\EN16931\DataType\ItemTypeCode;
+use Tiime\EN16931\Codelist\ItemTypeCodeUNTDID7143;
 
 class ItemClassificationIdentifier
 {
     public function __construct(
         public readonly string $value,
-        public readonly ItemTypeCode $scheme,
+        public readonly ItemTypeCodeUNTDID7143 $scheme,
         public readonly ?string $version = null,
     ) {
     }
--- src/DataType/Identifier/ObjectIdentifier.php
+++ src/DataType/Identifier/ObjectIdentifier.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Tiime\EN16931\DataType\Identifier;
 
-use Tiime\EN16931\DataType\ObjectSchemeCode;
+use Tiime\EN16931\Codelist\ReferenceQualifierCodeUNTDID1153;
 
 class ObjectIdentifier
 {
-    public function __construct(public readonly string $value, public readonly ?ObjectSchemeCode $scheme = null)
+    public function __construct(public readonly string $value, public readonly ?ReferenceQualifierCodeUNTDID1153 $scheme = null)
     {
     }
 }
